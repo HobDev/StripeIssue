e240b5410b3b056c4ad8ebed5d0c9fa8292d68e5
Paramjit <sparamjit92@gmail.com>
2024-12-27 16:23:20 +0530

    first commit

---
+++ StripeIssue/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace StripeIssue;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ StripeIssue/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace StripeIssue;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
